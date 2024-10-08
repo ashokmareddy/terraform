a16eef30d6f3301acd2ef4f5e73d98439bbbd0bb
ashokmareddy <tech.mareddy@gmail.com>
2024-10-08 17:01:05 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "google" {
+ credentials= file("gcp.json")
+project = "first-provider-437512-s4"
+  region  = "us-central1"
+zone="europe-west4-a"
+}
+
+resource "google_compute_instance" "centos_vm" {
+  name         = "my-vm2"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-stream-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
